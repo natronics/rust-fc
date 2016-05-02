fa082a40d3332075c88e77ae68b47532f9fee6c4
Nathan Bergey <nathan.bergey@gmail.com>
2016-05-01 21:36:03 -0600

    Docs: Build and deploy docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![doc(html_root_url = "https://natronics.github.io/rust-fc/")]
+
+//! # rust-fc
+//!
+//! A minimal clone of PSAS's [av3-fc](https://github.com/psas/av3-fc) rocket
+//! flight computer executive process written in Rust for fun.
+
 fn main() {
     println!("Launch!");
 }
