84965a2659f3adcccc338de55e7876176d3ed748
Nathan Bergey <nathan.bergey@gmail.com>
2016-05-02 14:49:08 -0600

    Listen on a port for UDP messages

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,39 @@
 //! A minimal clone of PSAS's [av3-fc](https://github.com/psas/av3-fc) rocket
 //! flight computer executive process written in Rust for fun.
 
+use std::net::UdpSocket;
+use std::io::Error;
+
+const PSAS_LISTEN_UDP_PORT: u16 = 36000;
+
+
+/// Listen on the data port for messages
+///
+/// Binds to the PSAS data port and listens for messages. Listening is blocking
+/// because if we're not receiving data, we do nothing (sleep).
+pub fn demux_udp() -> Result<(), Error> {
+
+    println!("Listening UDP ...");
+
+    // bind our socket to the PSAS data port:
+    let socket = try!(UdpSocket::bind(("0.0.0.0", PSAS_LISTEN_UDP_PORT)));
+
+    // A buffer to put data in from the port. At least size of MTU (1500 bytes)
+    let mut buf: [u8; 1500] = [0; 1500];
+
+    // Read from the port (blocking!)
+    // buf gets filled and we get the number of bytes read along with and
+    // address that the message came from
+    let (num_recv_bytes, recv_addr) = try!(socket.recv_from(&mut buf));
+
+    println!("Ping! Received {} bytes from port {}", num_recv_bytes, recv_addr.port());
+
+    Ok(())
+}
+
+
 fn main() {
     println!("Launch!");
+
+    demux_udp().unwrap();
 }
