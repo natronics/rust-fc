cea3e94597e558f312388ab9bd06d3fd4d393052
Nathan Bergey <nathan.bergey@gmail.com>
2016-05-18 23:05:38 -0600

    Simulation: Pack data from JSBSim into PSAS format and send along to flight computer

--- simulation/run.py
+++ simulation/run.py
@@ -4,12 +4,26 @@ import socket
 import time
 import sys
 import select
+from psas_packet import io, messages
+
+# data type
+ADIS = messages.MESSAGES['ADIS']
+
+# convert from stupid imperial units
+FPS2M = 0.3048
 
 
-# output from JSBSim goes to this socket (specified in output.xml)
+# output from JSBSim goes to this socket (port specified in output.xml)
 jsb_output = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 jsb_output.bind(("127.0.0.1", 5123))
 jsb_output.setblocking(0)
+
+
+# Socket writer for sending PSAS formated binary data to our flight computer
+fc_imu_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+fc_imu_sock.bind(('', 35020))
+fc_imu_sock.connect(('127.0.0.1', 36000))
+fc_imu = io.Network(fc_imu_sock)
 
 
 # Run JSBSim using Popen
@@ -27,7 +41,7 @@ p = subprocess.Popen([
 # settling time for JSBSim to initialize and start a TCP server for commands
 time.sleep(0.5)
 
-# Connect to JSBSim console with this socket (specified in run.xml <input port="5124" />
+# Connect to JSBSim console with this socket (specified in run.xml: <input port="5124" />
 jsb_console = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 jsb_console.connect(("127.0.0.1", 5124))
 
@@ -37,12 +51,13 @@ jsb_console.send("resume\n")
 
 # While loop for listening for data from JSBSim
 started = False
+seqn = 0
 while True:
     # Press any key at the command line to launch the rocket
     if not started and sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
 
         # the JSBSim command to ignite motor
-        jsb_console.send('set %s %s\r\n' % ("fcs/throttle-cmd-norm[0]", "1.0"))
+        jsb_console.send('set fcs/throttle-cmd-norm[0] 1.0\r\n')
         started = True  # so we only trigger this once
 
     # try reading data from JSBSim
@@ -56,13 +71,35 @@ while True:
             acc_W = float(data[3].strip())
 
             # uncomment to print data to console
-            print time, acc_U, acc_V, acc_W
+            #print time, acc_U, acc_V, acc_W
+
+            # Pack ADIS message
+            data = {
+                'VCC': 5.0,
+                'Gyro_X': 0.0,
+                'Gyro_Y': 0,
+                'Gyro_Z': 1,
+                'Acc_X': -acc_U * FPS2M,  # inverted to match IMU on rocket
+                'Acc_Y':  acc_V * FPS2M,
+                'Acc_Z':  acc_W * FPS2M,
+                'Magn_X': 0,
+                'Magn_Y': 0,
+                'Magn_Z': 0,
+                'Temp': 20,
+                'Aux_ADC': 0,
+            }
+
+            # send to FC and update seqn
+            fc_imu.send_data(ADIS, seqn, data)
+            seqn = seqn + 1
+
 
     # ctrl-c to kill the sim. Clean up after ourselves
     except KeyboardInterrupt, SystemExit:
         jsb_console.send("quit\n")
         jsb_console.close()
         jsb_output.close()
+        fc_imu_sock.close()
         p.kill()
         break
 
