737846d05854c52c82c0096158fa5eb1e500fdc9
Nathan Bergey <nathan.bergey@gmail.com>
2016-06-08 11:26:36 -0600

    Main: More cleanup, get rid of unnessisary match some, none{;}

--- src/main.rs
+++ src/main.rs
@@ -33,47 +33,44 @@ fn main() {
 
         // Wait for a message from the network
         // Then match it to the message type based on the port it came from
-        match flight_computer.listen() {
-            Some((seqn, recv_port, recv_time, message)) => {
-                match recv_port {
+        if let Some((seqn, recv_port, recv_time, message)) = flight_computer.listen() {
+            match recv_port {
 
-                    // Message from ADIS IMU
-                    // =====================
-                    // When we get new IMU data we log it. If it's a new message then we
-                    // update the state and send new data over the telemetry channel
-                    io::PSAS_ADIS_PORT => {
+                // Message from ADIS IMU
+                // =====================
+                // When we get new IMU data we log it. If it's a new message then we
+                // update the state and send new data over the telemetry channel
+                io::PSAS_ADIS_PORT => {
 
-                        // We expect monotonically increasing sequence numbers.
-                        // Anything received out of order is ignored. Real time
-                        // systems can't do anything with stale data!
-                        if seqn == (last_adis_message + 1) {
+                    // We expect monotonically increasing sequence numbers.
+                    // Anything received out of order is ignored. Real time
+                    // systems can't do anything with stale data!
+                    if seqn == (last_adis_message + 1) {
 
-                            // Unpack binary message into proper values with units
-                            let adis = devices::recv_adis(&message);
+                        // Unpack binary message into proper values with units
+                        let adis = devices::recv_adis(&message);
 
-                            // Since this is IMU data, we need to update the state vector
-                            state.update_imu(recv_time, adis);
+                        // Since this is IMU data, we need to update the state vector
+                        state.update_imu(recv_time, adis);
 
-                            // Log updated state
-                            flight_computer.log_message(&state.as_message(), state::STATE_NAME, recv_time, state::SIZE_OF_STATE).unwrap();
+                        // Log updated state
+                        flight_computer.log_message(&state.as_message(), state::STATE_NAME, recv_time, state::SIZE_OF_STATE).unwrap();
 
-                            // Log ADIS message and send it out over telemetry
-                            flight_computer.log_message(&message, devices::ADIS_NAME, recv_time, devices::SIZE_OF_ADIS).unwrap();
-                            flight_computer.telemetry(&message, devices::ADIS_NAME, recv_time, devices::SIZE_OF_ADIS);
-                        }
+                        // Log ADIS message and send it out over telemetry
+                        flight_computer.log_message(&message, devices::ADIS_NAME, recv_time, devices::SIZE_OF_ADIS).unwrap();
+                        flight_computer.telemetry(&message, devices::ADIS_NAME, recv_time, devices::SIZE_OF_ADIS);
+                    }
 
-                        // Update sequence number counter
-                        last_adis_message = seqn;
-                    },
+                    // Update sequence number counter
+                    last_adis_message = seqn;
+                },
 
 
-                    // Unknown Message Type
-                    // ====================
-                    // We don't know what message this is, skip it.
-                    _ => { ; }                  
-                }
-            },
-            None => { ; }  // Oh well. Keep listening.
+                // Unknown Message Type
+                // ====================
+                // We don't know what message this is, skip it.
+                _ => { ; }
+            }
         }
     }
 }
